a97ca0b3c6e698c1d61d5f2e197b027a3231cb36
Joel Perez <joelperezferrer@gmail.com>
2021-01-22 00:01:33 +0000

    Played around with Vue

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('vuetify', views.alt_index, name='vuetify'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+import random
+import json
+
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+
+    items = []
+    for i in range(5):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20, 80),
+            "url": "https://example.com",
+        })
+
+    context = {}
+    context["items"] = items
+    context["items_json"] = json.dumps(items)
+    return render(request, 'index.html', context)
+
+
+def alt_index(request):
+    return render(request, 'vuetify.html', {})
+
