aeb7b299bbab29d564aaf4370014370b1d1df707
Joel Perez <joelperezferrer@gmail.com>
2021-01-23 22:56:36 +0000

    Looks better now

--- home/views.py
+++ home/views.py
@@ -31,20 +31,30 @@ def index(request):
 
 def query(request, keyword, location):
     context = {}
-    context['keyword']: keyword
-    context['location']: location
-    '''
-    coords = Location.coordinates(location)
-
-    # Returns companies that are close
-    nearby = Company.objects.nearby(coords, limit=5)
-    context['companies'] = nearby
-    '''
+    context['keyword'] = keyword
+    context['location'] = location
     user_loc = Location.objects.create(post_code=location)
     context['companies'] = Company.objects.nearby(
         coords=[user_loc.coord_x, user_loc.coord_y], limit=3)
-    #print(context['companies'])
     user_loc.delete()
 
-    return render(request, 'query.html', context)
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = SearchForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            keyword = form.cleaned_data['keyword']
+            location = form.cleaned_data['location']
+            # redirect to a new URL:
+            url = '/query/' + keyword + '-' + location
+            return HttpResponseRedirect(url)
 
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    context['form'] = form
+
+    return render(request, 'query.html', context)
