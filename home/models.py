1be66e6e347322d23a755016184a42c09b2fcb47
Manuel Hurtado <manuelhurtadoram@gmail.com>
2021-01-23 11:15:46 -0500

    Created base models: Company, Location, Product.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+# Create LOCATION model
+class Location(models.Model):
+    street = models.CharField(max_length=200)
+    number = models.IntegerField()
+    other = models.IntegerField()
+    country = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=200)
+
+
+# Create PRODUCT model
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
+
+
+# Create COMPANY model
+class Company(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    bio = models.TextField(max_length=1000)
