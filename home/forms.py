93b4bc080290762aece4b3fbfb0924f9a33e93f7
Joel Perez <joelperezferrer@gmail.com>
2021-01-24 03:30:35 +0000

    Cuter

--- home/forms.py
+++ home/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class SearchForm(forms.Form):
     keyword = forms.CharField(label='I want to find the best', max_length=100)
     location = forms.CharField(label='in ', max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""  # Removes : as label suffix
