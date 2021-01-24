1413e565065ffa03afca2f9925e06dcf9ee348ee
Joel Perez <joelperezferrer@gmail.com>
2021-01-24 02:04:33 +0000

    Added update location action

--- home/admin.py
+++ home/admin.py
@@ -3,5 +3,18 @@ from .models import Location, Company, Product
 
 # Register your models here.
 admin.site.register(Location)
-admin.site.register(Company)
 admin.site.register(Product)
+
+
+def update_location(modeladmin, request, queryset):
+    for company in queryset:
+        company.location = Location.objects.get(pk=company.unique_id+'0')
+        company.save()
+update_location.short_description = "Update locations of the companies"
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location']
+    actions =[update_location]
+
+admin.site.register(Company, CompanyAdmin)
--- home/models.py
+++ home/models.py
@@ -30,7 +30,7 @@ class Location(models.Model):
             super(Location, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.post_code
+        return self.post_code + ' ' + self.unique_id
 
 
 # Create PRODUCT model
