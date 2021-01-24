cc6892ad7c082f4025ad57157a238d80f97452f0
Joel Perez <joelperezferrer@gmail.com>
2021-01-24 16:27:58 +0000

    Added admin function

--- home/admin.py
+++ home/admin.py
@@ -10,9 +10,23 @@ admin.site.register(Type)
 
 def update_location(modeladmin, request, queryset):
     for company in queryset:
-        company.location = Location.objects.get(pk=company.unique_id+'0')
+        company.location = Location.objects.get(pk=company.unique_id + '0')
         company.save()
+
+
 update_location.short_description = "Update locations of the companies"
+
+
+def remove_duplicate_tags(modeladmin, request, queryset):
+    for company in queryset:
+        company_types = company.type_set.all()
+        remain = []
+        for type in company_types:
+            if type.content not in remain:
+                remain.append(type.content)
+            else:
+                type.delete()
+remove_duplicate_tags.short_description = "Remove duplicate tags"
 
 
 class CompanyAdmin(admin.ModelAdmin):
@@ -24,7 +38,7 @@ class CompanyAdmin(admin.ModelAdmin):
     def get_y_coord(self, obj):
         return obj.location.coord_y
 
+    actions = [update_location, remove_duplicate_tags]
 
-    actions =[update_location]
 
 admin.site.register(Company, CompanyAdmin)
